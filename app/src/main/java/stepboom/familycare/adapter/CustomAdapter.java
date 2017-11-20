e12cc6d4dae3a53cf274f9ab3dd901e643424b0b
netklause <netklause@Nettokuns-MacBook-Pro.local>
2017-11-20 23:15:53 +0700

    net 1st try

---
+++ app/src/main/java/stepboom/familycare/adapter/CustomAdapter.java
@@ -0,0 +1,10 @@
+package stepboom.familycare.adapter;
+
+import android.widget.BaseAdapter;
+
+/**
+ * Created by netklause on 11/20/2017 AD.
+ */
+
+public class CustomAdapter extends BaseAdapter {
+}
